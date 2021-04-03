8852f927a04bb01d62378f7004065f82a0c5b230
Dwaraknaath Varadharajan <dwarakvaradharajan@gmail.com>
2021-04-03 08:27:58 -0400

    Modified Line chart

--- R/Dashboard_Menu/PredictTab/Back End/PredictTab_B.R
+++ R/Dashboard_Menu/PredictTab/Back End/PredictTab_B.R
@@ -5,10 +5,10 @@ addPredictServer <- function(id, data) {
   moduleServer(
     id,
     function(input, output, session) {
-      output$y_variable <- renderUI({
-        if (input$algo == "ARIMA") {
+      output$y <- renderUI({
+        if (input$algo == "Linear Regression") {
           choices <- unique(colnames(data()))
-          selectInput(session$ns("y_variable1"),
+          selectInput(session$ns("predictor"),
             "What to Predict",
             choices = choices,
             multiple = TRUE
@@ -16,43 +16,84 @@ addPredictServer <- function(id, data) {
         }
       })
 
-      output$y_variable <- renderUI({
-        if (input$algo == "ARIMA") {
+      output$x <- renderUI({
+        if (input$algo == "Linear Regression") {
           choices <- unique(colnames(data()))
-          selectInput(session$ns("y_variable1"),
-            "What to Predict",
+          selectInput(session$ns("variables"),
+            "Input Variables",
             choices = choices,
             multiple = TRUE
           )
         }
       })
 
-      output$P <- renderUI({
-        if (input$algo == "ARIMA") {
-          numericInput(session$ns("P1"),
-            h3("Auto Regressive Coeffcienct (P)"),
-            value = 1
-          )
+
+      Predict <- function(df2){
+        if (input$algo == "Linear Regression" && input$variables!=""){
+          # fit <- lm(data[[input$predictor]] ~ data[[input$variables[1]]])
+
+
+
+          # fit <- lm(df2[[input$predictor]] ~ df2[[input$variables[1]]])
+          #fit <- lm(as.formula(paste(input$predictor," ~ ",paste(input$variables,collapse="+"))),data=df2)
+          fit <- lm(y ~ ., data = df2)
+          print("Printing results")
+          print(summary(fit))
+          new <- data.frame(x = df2$x)
+          df <- predict(fit, new, se.fit = TRUE)
+          #
+          print("displaying results")
+          x1 <- data.frame(Input = df2$x,Predictor_variable = df2$y, Forecasted = df$fit)
+          print(x1)
+          return(x1)
+
+
         }
+
+
+
+      }
+
+      output$ForecastTable <- DT::renderDataTable({
+        df2 <- data() %>%
+          select(input$predictor, input$variables) %>%
+          group_by(!!!rlang::syms(input$variables)) %>%
+          summarise_all(funs(sum))
+        #
+        # data2<-data[complete.cases(data),]
+        # df2<-df1[complete.cases(df1),]
+        print(df2)
+        df2 <- df2 %>%
+           rename(y = input$predictor,
+            x = input$variables)
+
+        if (input$algo == "Linear Regression" && input$variables!=""){
+          # fit <- lm(data[[input$predictor]] ~ data[[input$variables[1]]])
+
+
+
+          # fit <- lm(df2[[input$predictor]] ~ df2[[input$variables[1]]])
+          #fit <- lm(as.formula(paste(input$predictor," ~ ",paste(input$variables,collapse="+"))),data=df2)
+          fit <- lm(y ~ ., data = df2)
+          print("Printing results")
+          print(summary(fit))
+          new <- data.frame(x = 1:length(df2$y))
+          df <- predict(fit, new, se.fit = TRUE)
+          #
+          print("displaying results")
+          x1 <- data.frame(Input = df2$x,Predictor_variable = df2$y, Forecasted = df$fit)
+
+        }
+        # Predict(df2)
+      },filter =  'top',
+      options = list(scrollX = TRUE)
+      )
+
+      data1 <- reactive({
+        Predict(data())
       })
 
-      output$Q <- renderUI({
-        if (input$algo == "ARIMA") {
-          numericInput(session$ns("Q1"),
-            h3("Moving Average Coeffcienct (P)"),
-            value = 1
-          )
-        }
-      })
 
-      output$D <- renderUI({
-        if (input$algo == "ARIMA") {
-          numericInput(session$ns("D1"),
-            h3("Differencing (D)"),
-            value = 1
-          )
-        }
-      })
 
       #  ARIMA(data(),input$y_variable1,input$P1,input$Q1,input$D1)
     }
